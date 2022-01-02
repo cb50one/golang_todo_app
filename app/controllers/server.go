c5b419d1a4032387417ca8c72eb544275b72d9c5
cb50one <txmalon0628@gmail.com>
2022-01-02 18:27:53 +0900

    add login

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -21,6 +21,8 @@ func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/authenticate", authenticate)
 	http.HandleFunc("/", top)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
